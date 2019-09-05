af82340dd9dc20471d9ee623bdc8be81365972aa
LenWinkler <lenwinkler86@gmail.com>
2019-09-05 17:32:42 -0400

    hit mvp. populated followersArray and used forEach to display cards

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,19 +3,17 @@
            https://api.github.com/users/<your name>
 */
 
-axios.get('https://api.github.com/users/LenWinkler')
-  .then(response => {
-    console.log(response);
-    let newCard = CardCreator(response);
-    cards.appendChild(newCard);
-    // response.data.forEach(item => {
-    //   let newCard = CardCreator(item);
-    //   cards.appendChild(newCard);
-    // });
-  })
-  .catch(error => {
-    console.log("Something went wrong =>", error);
-  });
+// had to move forEach below followersArray so that forEach could access it.
+
+//   followersArray.forEach(item => {
+//     axios.get(`https://api.github.com/users/${item}`)
+//   .then(response => {
+//     console.log(response);
+//     let newCard = CardCreator(response);
+//     cards.appendChild(newCard);
+//   });
+// });
+  
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -38,7 +36,16 @@ axios.get('https://api.github.com/users/LenWinkler')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ["lenwinkler", "bayronpuac", "ShrutiMalik123", "hsisco", "markpkng","afialydia",];
+
+followersArray.forEach(item => {
+  axios.get(`https://api.github.com/users/${item}`)
+.then(response => {
+  console.log(response);
+  let newCard = CardCreator(response);
+  cards.appendChild(newCard);
+});
+});
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
